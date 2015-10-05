4a26c74885ce2bf91d21e25972abbf18fab2ae23
Igor [Enkil] Timohin <tia@net-simple.ru>
2015-10-05 06:14:20 +0300

    Update 2015-10-05T03:14:14.533Z

--- js/custom/custom-bundle.js
+++ js/custom/custom-bundle.js
@@ -12,41 +12,50 @@ window.onload = function(){
      */
     var AMGLazyLoad = new LazyLoad({
         elements_selector: ".lazyload", // img
-        data_srcset: "original" // original-set
+        data_srcset: "original", // original-set
+        skip_invisible: false
     });
 
     // Call Owl-Carousel on header slider
-    $("#js-owl-top-slider").owlCarousel({
+    $(".js-owl-top-slider").owlCarousel({
         items: 1,
-        //center: true,
-        //loop: true,
         nav: true,
-        //autoWidth: true,
         dots: true,
         autoplay: true,
         autoplayTimeout: 4000,
         autoplayHoverPause: true,
         navContainer: ".owl-own-nav",
-        navText:[]
-    });
-
-    // Call Owl-Carousel on our works slider
-    $("#js-owl-works-slider").owlCarousel({
-        //items: 4,
-        //center: true,
-        //loop: false,
-        //nav: true,
-        //autoWidth: true,
-        //dots: false,
-        //autoplay: true,
-        //autoplayTimeout: 4000,
-        //autoplayHoverPause: true,
-        //navContainer: ".owl-own-nav",
-        //navText:[]
+        navText:[],
+        loop: true
     });
 
     // Center header slider dots
-    positionedAbsoluteElementHorizontalCenter('#js-owl-top-slider','.owl-dots');
+    positionedAbsoluteElementHorizontalCenter('.js-owl-top-slider','.owl-dots');
+
+    // Call Owl-Carousel on our works slider
+    $(".js-owl-works-slider").owlCarousel({
+        dots: false,
+        items: 4,
+        autoWidth: true,
+        margin: 30,
+        nav: true,
+        navContainer: ".owl-own-nav-works",
+        navText:[],
+        loop: true
+    });
+
+    // Call Owl-Carousel on partner slider
+    $(".js-owl-partners-slider").owlCarousel({
+        dots: false,
+        items: 4,
+        autoWidth: true,
+        margin: 40,
+        nav: true,
+        navContainer: ".owl-own-nav-partners",
+        navText:[],
+        loop: true
+    });
+
 };
 
 

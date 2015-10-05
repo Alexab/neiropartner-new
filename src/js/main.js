b744719e49a1abbe98e89fdf2154c1020a34a690
Igor [Enkil] Timohin <tia@net-simple.ru>
2015-10-05 10:34:05 +0300

    index 70%

--- src/js/main.js
+++ src/js/main.js
@@ -12,29 +12,49 @@ window.onload = function(){
      */
     var AMGLazyLoad = new LazyLoad({
         elements_selector: ".lazyload", // img
-        data_srcset: "original" // original-set
+        data_srcset: "original", // original-set
+        skip_invisible: false
     });
 
-    /**
-     * Call Owl-Carousel on header slider
-     */
-    $("#js-owl-top-slider").owlCarousel({
+    // Call Owl-Carousel on header slider
+    $(".js-owl-top-slider").owlCarousel({
         items: 1,
-        //center: true,
-        //loop: true,
         nav: true,
-        //autoWidth: true,
         dots: true,
         autoplay: true,
         autoplayTimeout: 4000,
         autoplayHoverPause: true,
         navContainer: ".owl-own-nav",
-        navText:[]
+        navText:[],
+        loop: true
     });
 
-    /**
-     * Center header slider dots
-     */
-    positionedAbsoluteElementHorizontalCenter('#js-owl-top-slider','.owl-dots');
+    // Center header slider dots
+    positionedAbsoluteElementHorizontalCenter('.js-owl-top-slider','.owl-dots');
+
+    // Call Owl-Carousel on our works slider
+    $(".js-owl-works-slider").owlCarousel({
+        dots: false,
+        items: 4,
+        //autoWidth: true,
+        margin: 30,
+        nav: true,
+        navContainer: ".owl-own-nav-works",
+        navText:[],
+        loop: true
+    });
+
+    // Call Owl-Carousel on partner slider
+    $(".js-owl-partners-slider").owlCarousel({
+        dots: false,
+        items: 4,
+        //autoWidth: true,
+        margin: 40,
+        nav: true,
+        navContainer: ".owl-own-nav-partners",
+        navText:[],
+        loop: true
+    });
+
 };
 

669b59277d1a0955137c28bf847b20902a133ede
Igor [Enkil] Timohin <timohin.i@gmail.com>
2015-10-20 06:44:26 +0300

    Update 2015-10-20T03:43:18.847Z

--- js/custom/custom-bundle.js
+++ js/custom/custom-bundle.js
@@ -39,6 +39,11 @@ window.onload = function(){
     // Call Owl-Carousel on partners block if there os more than 4 works
     setOwlCarouselIfMoreThen(".js-owl-partners-slider", 4, ".owl-own-nav-partners");
 
+    // Call Owl-Carousel on video block if there os more than 4 works
+    setOwlCarouselIfMoreThen(".js-owl-video-slider", 3, ".owl-own-nav-video");
+
+    // Find html5 video and activate
+    //videoHTML5Youtube();
 };
 
 
@@ -79,7 +84,7 @@ function setOwlCarouselIfMoreThen(container, count, navContainer) {
                             margin: 10
                         },
                         // breakpoint from 740 up
-                        740 : {
+                        770 : {
                             items: 2,
                             margin: 10
                         },
@@ -94,3 +99,32 @@ function setOwlCarouselIfMoreThen(container, count, navContainer) {
         }
     }
 }
+
+
+var videoHTML5Youtube = function () {
+    var videos = document.querySelectorAll("video");
+    for (var i = 0, l = videos.length; i < l; i++) {
+        var video = videos[i];
+        var src = video.src || (function () {
+                var sources = video.querySelectorAll("source");
+                for (var j = 0, sl = sources.length; j < sl; j++) {
+                    var source = sources[j];
+                    var type = source.type;
+                    var isMp4 = type.indexOf("mp4") != -1;
+                    if (isMp4) return source.src;
+                }
+                return null;
+            })();
+        if (src) {
+            var isYoutube = src && src.match(/(?:youtu|youtube)(?:\.com|\.be)\/([\w\W]+)/i);
+            if (isYoutube) {
+                var id = isYoutube[1].match(/watch\?v=|[\w\W]+/gi);
+                id = (id.length > 1) ? id.splice(1) : id;
+                id = id.toString();
+                var mp4url = "http://www.youtubeinmp4.com/redirect.php?video=";
+                video.src = mp4url + id;
+            }
+        }
+    }
+};
+

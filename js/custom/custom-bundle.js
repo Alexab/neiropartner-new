066658c201abbd0a5aec221a514bc171d1806e76
Igor [Enkil] Timohin <timohin.i@gmail.com>
2016-01-04 10:09:53 +0300

    Update 2016-01-04T07:09:49.897Z

--- js/custom/custom-bundle.js
+++ js/custom/custom-bundle.js
@@ -89,6 +89,16 @@ window.onload = function(){
 
 
 /**
+ * Toggler for gamburger menu in top header
+ * @type {Element}
+ */
+var toggler = document.getElementById('js-toggler');
+toggler.onclick = function(e){
+    e.preventDefault();
+    toggler.classList.toggle('header-toggler--close');
+    document.getElementById('js-top-nav').classList.toggle('header__nav--visible');
+};
+/**
  * Centering element with position: absolute in container block
  */
 function positionedAbsoluteElementHorizontalCenter(containerSelector,elementSelector){
